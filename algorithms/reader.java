fe81b3338fba6c0f6ceec5842db3366704eaaf17
Favarela <favarela@itba.edu.ar>
2016-10-30 13:48:56 -0300

    Create reader.java

---
+++ algorithms/reader.java
@@ -0,0 +1,25 @@
+package TPE;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Reader {
+	
+	public String readTxt(String direction){ 
+			
+			String text = "";
+			
+			try{
+				BufferedReader bf = new BufferedReader(new FileReader(direction));
+				String temp = "";
+				String bfRead;
+				while((bfRead= bf.readLine())!= null){ 
+					temp = temp+bfRead+"*";
+				}
+				text=temp;
+				bf.close();
+			}catch (Exception e){ 
+				System.err.println("No se encontro Archivo");
+			}
+			return text;
+		}
+}
