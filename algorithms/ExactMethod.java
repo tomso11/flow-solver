52f430584857794f2dacf6dae03913976d590951
Favarela <favarela@itba.edu.ar>
2016-11-05 02:04:42 -0300

    Create ExactMethod.java

---
+++ algorithms/ExactMethod.java
@@ -0,0 +1,74 @@
+package algorithms;
+
+import java.awt.Point;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExactMethod {
+	
+	
+	public ExactMethod() {
+	}
+
+	public void ExactSolution(Tablero tab, boolean print){
+		long time = System.currentTimeMillis();
+		Set<Tablero> solutions = new HashSet<Tablero>();
+		Set<Tablero> parcialSolutions = new HashSet<Tablero>();
+		solutions.add(tab);
+		Tablero bestSolution = tab;
+		int bestSolutionEmpty = (tab.getFils()*tab.getCols())-(tab.getColours()*2);
+		Point[] sink = tab.getSinks();
+		
+		for (int i = 0 ; i < tab.getColours(); i+=2 ){
+			for (Tablero each: solutions){
+				parcialSolutions.addAll(findPath(each, sink[i], sink[i+1],tab.getColour(i/2),print));
+			}
+			solutions= parcialSolutions;
+			parcialSolutions = null;
+		}
+		
+		for (Tablero each : solutions ){
+			int eachEmpty = each.cellsEmpty();
+			if ( eachEmpty < bestSolutionEmpty){
+				bestSolution = each;
+				bestSolutionEmpty = eachEmpty;
+			}
+		}
+		
+		for ( int j = 0 ; j < tab.getFils(); j++ ){
+				System.out.println(bestSolution.getTablero()[j]);
+		}
+		
+		time = System.currentTimeMillis() - time;
+		System.out.println("El algoritmo encontro la solucion exacta en " + time + " segundos.");
+		return;
+	}
+
+	public Set<Tablero> findPath(Tablero tablero,Point origin, Point destiny, int colour, boolean print){
+		Set<Tablero> solutions = new HashSet<Tablero>();
+		int [] movement = {-1,0,0,1,1,0,0,-1};		
+		if (origin == destiny){
+			solutions.add(tablero);
+			return solutions;
+		}
+		
+		for (int i = 0 ; i < 8 ; i+=2){
+			Point cell = new Point ((int)origin.getX(),(int)origin.getY());
+			cell.translate(movement[i], movement[i+1]);
+			if( tablero.cellIsEmpty(cell)){
+				if (print){
+					MainFrame draw = new MainFrame(tablero.getFils(),tablero.getCols(),tablero);
+					draw.setVisible(true);
+					try{
+						Thread.sleep(100);
+					}catch(InterruptedException ex){
+						Thread.currentThread().interrupt();
+					}
+				}
+				tablero.paintCell(cell,colour);
+				solutions.addAll(findPath(tablero,cell,destiny,colour,print));
+			}
+		}
+		return solutions;
+	}
+}
