a8507573a537cd75b2e16b58f22057e59bcbddf5
tsoracco <tomassoracco@gmail.com>
2016-11-06 18:26:58 -0300

    Algoritmo de Lee
    Funcionando para 1 color en 3x3

--- algorithms/HCInput.java
+++ algorithms/HCInput.java
@@ -137,7 +137,8 @@ public class HCInput {
 		int[] colors={0};
 		TableroControl tab=new TableroControl(grid, 3, 3, colors, sinks);
 		HCInput input=new HCInput(tab,sinks);
-		input.quickSolution();
+		//input.quickSolution();
+		input.queueSolution();
 		
 	}
 	
@@ -148,59 +149,78 @@ public class HCInput {
 		for(int i=0; i< sinks.length ; i++){ 
 			Cell c=new Cell(sinks[i].getFirstX(),sinks[i].getFirstY(),sinks[i].getColor() );
 			Cell tgt=new Cell(sinks[i].getSecX(),sinks[i].getSecY(),sinks[i].getColor() );
+			System.out.println("las coordenadas de tgt son x: "+tgt.getX()+ " y: "+tgt.getY() );
 			queue.add(c);
 			while(!(queue.isEmpty()) ){
+				System.out.println("vamos otra vez");
 				Cell current= queue.poll();
-				Cell[] neighbors=getNeighbors(current, grid);
-				for(int j=0; j< neighbors.length && !found; j++){
-					if(neighbors[j].equals(tgt)){
-						grid=markPath(grid, tgt);
-						found=true;
-						queue=emptyQ(queue);
+				Cell[] neighbors=getNeighbors(current, grid, tgt);
+				if(neighbors[0] != null){
+					for(int j=0; j< neighbors.length && !found; j++){
+						System.out.println("las coordenadas de este neigh son x: "+neighbors[j].getX()+ " y: "+neighbors[j].getY() );
+						if(neighbors[j].equals(tgt)){
+							grid=markPath(grid, tgt, c);
+							found=true;
+							queue=emptyQ(queue);
+						}
+						else{
+							queue.add(neighbors[j]);
+							System.out.println("encolo");
+						}
 					}
-					else{
-						queue.add(neighbors[j]);
-					}
-				}
-				
+				} // falta un else ?
 			}
 			grid=clearGrid(grid);
 			queue=emptyQ(queue);
+			printMatrix(grid);
 		}
 		return grid;
 	}
 
-	private Cell[] getNeighbors(Cell current, int[][] grid) {
-		Cell[] arr = null;
+	private Cell[] getNeighbors(Cell current, int[][] grid, Cell tgt) {
+		Cell[] arr = new Cell[3];
 		int n=0;
 		int x1=current.getX(),y1=current.getY();
+		int x2=tgt.getX(), y2=tgt.getY();
 		if( x1 != 0 && grid[x1-1][y1] == -1){
+			//System.out.println("el color es "+current.getColor());
 			grid[x1-1][y1]=(current.getColor() >= 10)? (current.getColor()+1):(10); // si la anterior tiene numero le agrego peso, sino, comienzo el camino 
-			arr[n++]=new Cell(x1-1,y1,current.getColor()+1);
+			arr[n++]=new Cell(x1-1,y1,(current.getColor() >= 10)? (current.getColor()+1):(10));
 			printMatrix(grid);
 		}
 		if( x1 != tab.getX()-1 && grid[x1+1][y1]== -1){
+			//System.out.println("el color es "+current.getColor());
 			grid[x1+1][y1]=(current.getColor() >= 10)? (current.getColor()+1):(10);
-			arr[n++]=new Cell(x1+1,y1,current.getColor()+1);
+			arr[n++]=new Cell(x1+1,y1,(current.getColor() >= 10)? (current.getColor()+1):(10));
 			printMatrix(grid);
-		}printMatrix(grid);
+		}
 		if( y1 != 0 && grid[x1][y1-1]== -1){
+			//System.out.println("el color es "+current.getColor());
 			grid[x1][y1-1]=(current.getColor() >= 10)? (current.getColor()+1):(10);
-			arr[n++]=new Cell(x1,y1-1,current.getColor()+1);
+			arr[n++]=new Cell(x1,y1-1,(current.getColor() >= 10)? (current.getColor()+1):(10));
 			printMatrix(grid);
 		}
 		if( y1 != tab.getY()-1 && grid[x1][y1+1]== -1){
+			//System.out.println("el color es "+current.getColor());
 			grid[x1][y1+1]=(current.getColor() >= 10)? (current.getColor()+1):(10);
-			arr[n++]=new Cell(x1,y1+1,current.getColor()+1);
+			arr[n++]=new Cell(x1,y1+1,(current.getColor() >= 10)? (current.getColor()+1):(10));
 			printMatrix(grid);
+		}
+		if ( (x1-1== x2 && y1 == y2) || (x1+1== x2 && y1 == y2) || (x1== x2 && y1-1 == y2) || (x1== x2 && y1+1 == y2)){
+			System.out.println("found tgt!");
+			arr[n++]=new Cell(x2,y2,tgt.getColor());
 		}
 		return arr;
 	}
 
-	private int[][] markPath(int[][] grid, Cell tgt) {
+	private int[][] markPath(int[][] grid, Cell tgt, Cell source) {
 		int x=tgt.getX(),y=tgt.getY();
-		Cell min = new Cell(-1,-1,tab.getX() * tab.getY()); // celda a la que nos vamos a mover
-		while( grid[x][y] >= 10 || ( x==tgt.getX() && y==tgt.getY() ) ){
+		int x2=source.getX(), y2=source.getY();
+		int pathLength=0;
+		Cell min = new Cell(-1,-1,tab.getX() * tab.getY()+10); // celda a la que nos vamos a mover
+		System.out.println("las coordenadas de este tgt son x: "+tgt.getX()+ " y: "+tgt.getY() );
+		while( min.getX() != x2 && min.getY() != y2){ //grid[x][y] >= 10 || ( x==tgt.getX() && y==tgt.getY() ) 
+			System.out.println("dentro del path");
 			if( x != 0 && grid[x-1][y]>=10 ){
 				if( grid[x-1][y] < min.getColor() ){
 					min.setX(x-1);
@@ -230,10 +250,15 @@ public class HCInput {
 				}
 			}
 			printMatrix(grid);
-			grid[x][y]=tgt.getColor(); // como los colores van de 0 a 9 cuando llegue a 10 y lo pinte cortara la iteracion
+			System.out.println(tgt.getColor());
+			grid[min.getX()][min.getY()]=tgt.getColor(); // como los colores van de 0 a 9 cuando llegue a 10 y lo pinte cortara la iteracion
 			x=min.getX();
 			y=min.getY();
+			System.out.println("las coordenadas de current son x: "+min.getX()+ " y: "+min.getY() +" y su color "+min.getColor());
+			printMatrix(grid);
+			pathLength++;
 		}
+		System.out.println("largo del camino: "+ pathLength);
 		return grid;
 		
 	}
