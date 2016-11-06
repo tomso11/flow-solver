d8c71c80c6ac637188c6ccf8af63ccd581ad71e8
Favarela <favarela@itba.edu.ar>
2016-11-05 21:04:15 -0300

    Update Game.java

--- algorithms/Game.java
+++ algorithms/Game.java
@@ -1,5 +1,3 @@
-package algorithms;
-
 public class Game {
 	public static void main(String[] args) {
 		String exact= "exact";
@@ -7,38 +5,46 @@ public class Game {
 		String progress= "progress";
 		Reader a = new Reader();
 		Solver s = new Solver();
-		String text = a.readTxt(args[0]);
 		VerifyInput game = new VerifyInput();
-		game.input(text);
+		String text = a.readTxt("C:\\Users\\flor\\Desktop\\Facu\\Eclipse\\flow-solver\\algorithms\\Tablero.txt");
+		if (game.input(text)){
 		int[][] table = VerifyInput.getTable();
 		System.out.println("Free Flow Game Solver!");
 		System.out.println("Beggining GameBoard: \n");
-		for ( int j = 0 ; j < 6; j++ ){
-				System.out.println(table[j]);
+		for ( int j = 0 ; j < 6 ; j++ ){
+			for (int i = 0 ; i < 6 ; i++){
+				System.out.println(table[j][i]);
 			}
+		}
         	MainFrame window = new MainFrame(game.getFils(),game.getCols(),VerifyInput.getTable());
         	window.setVisible(true);
-		if (args[1]!=null && args[1].equals(exact)){
-			if (args[2]!=null && args[2].equals(progress)){
-				s.solExact(true);
-			}else if (args[2]==null){
-				s.solExact(false);
-			}else throw new  IllegalArgumentException();
+        	
+/*
+* 		if (args[1]!=null && args[1].equals(exact)){
+*
+*			if (args[2]!=null && args[2].equals(progress)){
+*				s.solExact(true);
+*			}else if (args[2]==null){
+*				s.solExact(false);
+*			}else throw new  IllegalArgumentException();
+*		}
+*		else if (args[1]!=null && args[1].equals(approx)){
+*			Integer time;
+*			if (args[2]!=null && isInt(args[2])){
+*				time=Integer.parseInt(args[2]);
+*				if(args[3]!=null && args[3].equals(progress)){
+*					s.solAprox(true, time);
+*				}else if(args[3]==null){
+*					s.solAprox(false, time);
+*				}else throw new  IllegalArgumentException();
+*			}else throw new  IllegalArgumentException();
+*			
+*		}else throw new IllegalArgumentException();
+*/		}else{
+			return;
 		}
-		else if (args[1]!=null && args[1].equals(approx)){
-			Integer time;
-			if (args[2]!=null && isInt(args[2])){
-				time=Integer.parseInt(args[2]);
-				if(args[3]!=null && args[3].equals(progress)){
-					s.solAprox(true, time);
-				}else if(args[3]==null){
-					s.solAprox(false, time);
-				}else throw new  IllegalArgumentException();
-			}else throw new  IllegalArgumentException();
-			
-		}else throw new IllegalArgumentException();
-		
-	}
+}
+
 
 		
 		public static boolean isInt(String str) {
