534cc311c46f27c503811573380c1b6a4cca74e8
Gabriel Silvatici <gsilvati@itba.edu.ar>
2016-11-11 06:33:57 -0300

    Update ApproxMethod.java

--- algorithms/ApproxMethod.java
+++ algorithms/ApproxMethod.java
@@ -8,48 +8,59 @@ public class ApproxMethod {
 		
 	}
 	
-	public void approxSolution(TableroControl tab, long time, boolean progress){
+		public void approxSolution(TableroControl tab, long time, boolean progress){
 		TableroControl tabClimb= tab;
 		TableroControl tabClimbMax = tab;
 		long timeBest=0;
 		Lee lee=new Lee();
 		long took=0;
-		tabClimb=lee.getSolution(tabClimb,time,progress);
+		AtomicInteger t = new AtomicInteger((int)time);
+		tabClimb=lee.getSolution(tabClimb,t,progress);
 		long end;
-		long start=System.currentTimeMillis();
+		long start=System.nanoTime();
 		long auxT1 = 0, auxT2 = 0;
+		
 		while(time > took && !tab.isSolved() ){
-			System.out.println("enter");
+			System.out.println(took);
 			// Escala hacia la solucion
-			// Falta agregarle el progress y time
-			auxT1 = System.currentTimeMillis();
+			auxT2 = System.nanoTime(); 
 			tabClimb=HillClimb.climbSolution(tabClimb.getTablero(), tabClimb.getX(), tabClimb.getY(), tabClimb.getSinks(), progress);
-			auxT2 = System.currentTimeMillis();
-			// Me dijo se el metodo climbSolution me tomo mas tiempo del permitido dentro del ciclo
-			if(auxT2 - auxT1 + took >= time) {
-				break;
-			}
 			// Si algun camino no estan conectado corre el Lee de nuevo para ver si puede conectar los que falta 
 			if(!tabClimb.isPathsConected()) {
-				auxT1 = System.currentTimeMillis();
-				tabClimb=lee.getSolution(tabClimb,time,progress);
-				auxT2 = System.currentTimeMillis();
-				// Me fijo si el metodo getSolution me tomo mas tiempo del permitido dentro del ciclo
-				if(auxT2 - auxT1 + took >= time) {
-					break;
-				}
+				System.out.println(timeBest);
+				t.set((int)(time - took));
+				tabClimb=lee.getSolution(tabClimb,t,progress);
+				took+=t.get();
 				tabClimbMax=tabClimb;
 			// Si estan todos conectados pregunta si el puntaje de la ultima escalada es mejor que el de maximo registrado
 			} else {
-				if(tabClimb.getScore() >= tabClimbMax.getScore()) {
-					tabClimbMax = tabClimb;
+				System.out.println("sfsagas");
+				if(timeBest == 0) {
 					timeBest=took;
 				}
+				auxT1 = System.nanoTime();
+				if((auxT2 - auxT1)/1000000 + took >= time) {
+					break;
+				}
+				if(tabClimb.getScore() >= tabClimbMax.getScore()) {
+					tabClimbMax = tabClimb;
+				}
 			}
-
-			end=System.currentTimeMillis();
-			took=end-start;
+			if(progress){
+				MainFrame draw = new MainFrame(tabClimb.getX(),tabClimb.getY(),tabClimb.getTablero());
+				draw.setVisible(true);
+				try{
+					Thread.sleep(200);
+				}catch(InterruptedException ex){
+					Thread.currentThread().interrupt();
+				}
+				draw.dispose();
+			}
+			end= System.nanoTime();
+			took=(end-start)/1000000;
 		}
+		MainFrame draw = new MainFrame(tabClimbMax.getX(),tabClimbMax.getY(),tabClimbMax.getTablero());
+		draw.setVisible(true);
 		System.out.println("Approx Solution GameBoard:\n");
 		for (int x=0; x < tab.getX(); x++) {
 			System.out.print("|");
@@ -59,22 +70,11 @@ public class ApproxMethod {
 			}
 			System.out.println("|");
 		}
-		System.out.println("\nThe algorithm found the exact solution in " + timeBest/60000 + " minutes," + (timeBest%60000)/1000 + " seconds and " + (timeBest%1000) +" milliseconds.");
+		if (timeBest != 0) {
+			System.out.println("\nThe algorithm found a solution in " + timeBest/60000 + " minutes," + (timeBest%60000)/1000 + " seconds and " + (timeBest%1000) +" milliseconds.");			
+		}
 	}
 
-	public static void main(String[] args) {
-		int[][] grid ={{1, 0, -1, -1, -1},{-1, -1, -1, -1, -1},{-1, -1, -1, -1, -1}, {-1,-1,-1,-1,-1} , {-1,-1,-1,-1,-1}, {2,1,-1,2,0}};
-		Sink sink=new Sink(0,1,5,4,0);
-		Sink sink2=new Sink(0,0,5,1,1);
-		Sink sink3=new Sink(5,0,5,3,2);
-		LinkedList<Sink> sinks=new LinkedList<Sink>();
-		sinks.add(sink);
-		sinks.add(sink2);
-		sinks.add(sink3);
-		int[] colors={0,1,2};
-		TableroControl tab=new TableroControl(grid, 6, 5, colors, sinks);
-		ApproxMethod app = new ApproxMethod();
-		long time=30 * 10000;
-		app.approxSolution(tab, time, false);
-	}
+
+	
 }
